fc703f0eac6dd102d861217da3a37d3efddcc0d9
jefftorcato <jefftorcato@gmail.com>
2019-11-17 15:07:51 +0530

    variables

--- SaudadeAdmin/app/src/main/java/com/jefftorcato/saudadeadmin/ui/home/HomeFragment.kt
+++ SaudadeAdmin/app/src/main/java/com/jefftorcato/saudadeadmin/ui/home/HomeFragment.kt
@@ -1,27 +1,54 @@
 package com.jefftorcato.saudadeadmin.ui.home
 
 import android.os.Bundle
+import android.os.RecoverySystem
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import com.jefftorcato.saudadeadmin.R
+import com.jefftorcato.saudadeadmin.data.adapter.EventAdapter
 import com.jefftorcato.saudadeadmin.databinding.FragmentHomeBinding
+import com.jefftorcato.saudadeadmin.ui.dialog.FilterDialogFragment
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.x.kodein
 import org.kodein.di.generic.instance
 
-class HomeFragment : Fragment(),KodeinAware {
+class HomeFragment : Fragment(),
+    KodeinAware,
+    View.OnClickListener,
+    FilterDialogFragment.FilterListener,
+    EventAdapter.OnEventSelectedListener {
 
     override val kodein by kodein()
-    private val factory : HomeViewModelFactory by instance()
+    private val factory: HomeViewModelFactory by instance()
     private lateinit var homeViewModel: HomeViewModel
 
     private var mToolbar: Toolbar? = null
+    private lateinit var mCurrentSearchView: TextView
+    private lateinit var mCurrentSortByView: TextView
+    private lateinit var mEventsRecycler: RecyclerView
+    private lateinit var mEmptyView: ViewGroup
+
+    private lateinit var mFirestore: FirebaseFirestore
+    private lateinit var mQuery: Query
+
+    private lateinit var mFilterDialog: FilterDialogFragment
+    private lateinit var mAdapter: EventAdapter
+
+    companion object {
+        private val TAG: String = "MainActivity"
+        private val LIMIT: Int = 50
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,17 +56,13 @@ class HomeFragment : Fragment(),KodeinAware {
         savedInstanceState: Bundle?
     ): View? {
         homeViewModel =
-            ViewModelProviders.of(this,factory).get(HomeViewModel::class.java)
-        val binding: FragmentHomeBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_home,container,false)
+            ViewModelProviders.of(this, factory).get(HomeViewModel::class.java)
+        val binding: FragmentHomeBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
         (activity as AppCompatActivity).supportActionBar!!.hide()
+
         binding.viewmodel = homeViewModel
         mToolbar = binding.root.findViewById(R.id.toolbar)
-
-
-        //val textView: TextView = root.findViewById(R.id.text_home)
-        /*homeViewModel.text.observe(this, Observer {
-            textView.text = it
-        })*/
 
         return binding.root
     }

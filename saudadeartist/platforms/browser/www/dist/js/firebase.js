1161674ac75c717cd6c2f81ae76826cd9e4bf0e1
jefftorcato <jefftorcato@gmail.com>
2019-11-24 13:11:18 +0530

    some stuff

--- saudadeartist/platforms/browser/www/dist/js/firebase.js
+++ saudadeartist/platforms/browser/www/dist/js/firebase.js
@@ -1,17 +1,5 @@
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyDjLQZNX4ZIRKKW2doEEihSNOeWYuCI5Pk",
-    authDomain: "saudade-ab518.firebaseapp.com",
-    databaseURL: "https://saudade-ab518.firebaseio.com",
-    projectId: "saudade-ab518",
-    storageBucket: "saudade-ab518.appspot.com",
-    messagingSenderId: "501133640393",
-    appId: "1:501133640393:web:dc36ce92ad78a238ca4318",
-    measurementId: "G-7C8CMWSV9T"
-  };
-
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-  var db = firebase.firestore();
-  var storageRef = firebase.storage().ref();
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+var db = firebase.firestore();
+var storageRef = firebase.storage().ref();
--- saudadeartist/www/dist/js/add-event/add-event.js
+++ saudadeartist/www/dist/js/add-event/add-event.js
@@ -8,14 +8,18 @@
             $("#inputCategory").append('<option value="' + element.id +'">'+ element.name +'</option>');
         });
     });*/
-    db.collection("eventCategory")
-        .onSnapshot(function (querySnapshot) {
-            querySnapshot.forEach(function (doc) {
-                // doc.data() is never undefined for query doc snapshots
-                //console.log(doc.id, " => ", doc.data().name);
-                $("#inputCategory").append('<option value="' + doc.id + '">' + doc.data().name + '</option>');
-            });
-        })
+
+    $(document).ready(function () {
+        db.collection("eventCategory")
+            .onSnapshot(function (querySnapshot) {
+                querySnapshot.forEach(function (doc) {
+                    // doc.data() is never undefined for query doc snapshots
+                    //console.log(doc.id, " => ", doc.data().name);
+                    $("#inputCategory").append('<option value="' + doc.data().name + '">' + doc.data().name + '</option>');
+                });
+            })
+    })
+
 
     $('#form-addEvent').submit(function (event) {
         event.preventDefault();
@@ -27,70 +31,83 @@
 
         let form_data = {
             artist: uid,
-            eventGroup: $('#inputCategory').val(),
-            eventLocation: $('#inputEventlocation').val(),
-            eventName: $('#inputEventname').val(),
+            avgRating: 0.0,
+            category: $('#inputCategory').val(),
+            city: $('#inputEventlocation').val(),
+            name: $('#inputEventname').val(),
+            numRatings: 0,
             ticketCount: parseInt($('#inputEventticket').val()),
-            pathToImage: ""
+            photo: ""
         }
 
         console.log(form_data)
-
-        const selectedFile = document.getElementById('inputEventposter').files[0];
-
-        // Upload file and metadata to the object 'images/image-name.ext'
-        var uploadTask = storageRef.child('images/' + selectedFile.name).put(selectedFile);
-        // Listen for state changes, errors, and completion of the upload.
-        uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
-            function (snapshot) {
-                // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-                var progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
-                //console.log('Upload is ' + progress + '% done');
-                document.getElementById('upload-msg').textContent = progress + '%'
-                switch (snapshot.state) {
-                    case firebase.storage.TaskState.PAUSED: // or 'paused'
-                        //console.log('Upload is paused');
-                        break;
-                    case firebase.storage.TaskState.RUNNING: // or 'running'
-                        //console.log('Upload is running');
-                        break;
-                }
-            },
-            function (error) {
-
-                // A full list of error codes is available at
-                // https://firebase.google.com/docs/storage/web/handle-errors
-                switch (error.code) {
-                    case 'storage/unauthorized':
-                        // User doesn't have permission to access the object
-                        break;
-
-                    case 'storage/canceled':
-                        // User canceled the upload
-                        break;
-
-                    case 'storage/unknown':
-                        // Unknown error occurred, inspect error.serverResponse
-                        break;
-                }
-            },
-            function () {
-                // Upload completed successfully, now we can get the download URL
-                uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
-                    console.log('File available at', downloadURL);
-                    form_data.pathToImage = downloadURL;
-                    db.collection("event").add(form_data)
-                        .then(function (docRef) {
-                            console.log("Document written with ID: ", docRef.id);
-                            $inputs.prop("disabled", false);
-                            $('#form-addEvent')[0].reset();
-                            overlayOff();
-                        })
-                        .catch(function (error) {
-                            console.error("Error adding document: ", error);
-                        });
-                });
+        let eventEntry = db.collection("event").add(form_data)
+            .then(function (docRef) {
+                console.log("Document written with ID: ", docRef.id);
+                uploadImage(docRef.id)
+            })
+            .catch(function (error) {
+                console.error("Error adding document: ", error);
             });
+
+        function uploadImage(docRef) {
+            const selectedFile = document.getElementById('inputEventposter').files[0];
+
+            // Upload file and metadata to the object 'images/image-name.ext'
+            var uploadTask = storageRef.child('images/' + docRef).put(selectedFile);
+            // Listen for state changes, errors, and completion of the upload.
+            uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+                function (snapshot) {
+                    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                    var progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+                    //console.log('Upload is ' + progress + '% done');
+                    document.getElementById('upload-msg').textContent = progress + '%'
+                    switch (snapshot.state) {
+                        case firebase.storage.TaskState.PAUSED: // or 'paused'
+                            //console.log('Upload is paused');
+                            break;
+                        case firebase.storage.TaskState.RUNNING: // or 'running'
+                            //console.log('Upload is running');
+                            break;
+                    }
+                },
+                function (error) {
+
+                    // A full list of error codes is available at
+                    // https://firebase.google.com/docs/storage/web/handle-errors
+                    switch (error.code) {
+                        case 'storage/unauthorized':
+                            // User doesn't have permission to access the object
+                            break;
+
+                        case 'storage/canceled':
+                            // User canceled the upload
+                            break;
+
+                        case 'storage/unknown':
+                            // Unknown error occurred, inspect error.serverResponse
+                            break;
+                    }
+                },
+                function () {
+                    // Upload completed successfully, now we can get the download URL
+                    uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
+                        console.log('File available at', downloadURL);
+                        form_data.photo = downloadURL;
+                        db.collection("event").doc(docRef).set({
+                                photo: form_data.photo
+                            }, { merge: true })
+                            .then(function () {
+                                $inputs.prop("disabled", false);
+                                $('#form-addEvent')[0].reset();
+                                overlayOff();
+                            })
+                            .catch(function (error) {
+                                console.error("Error adding document: ", error);
+                            });
+                    });
+                });
+        }
     })
 
 })(jQuery); // End of use strict
